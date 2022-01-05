c6b4075221380e6b8b385f4ba88c8a06c91d7387
Home8524 <jeby1280@naver.com>
2022-01-05 11:48:55 +0900

    update

--- 2022.01.04/Assets/FollowCamera.cs
+++ 2022.01.04/Assets/FollowCamera.cs
@@ -7,35 +7,81 @@ public class FollowCamera : MonoBehaviour
 
     public GameObject Target;
 
-    [SerializeField] private float SmoothTime;
-
     [SerializeField] private Vector3 Offset;
 
+    //카메라의 최대 줌 위치
+    [SerializeField] private Vector3 MaxPoint;
+    [SerializeField] private Vector3 MinPoint;
+    //카메라 속도
     private Vector3 Velocity;
 
-    private float ZoomDistance;
 
     private bool ShakeCamera;
 
+    private LayerMask PlayerMask;
+
     [Range(0.0f,1.0f)]
     private float Radius;
+
+    [Range(0.0f,1.0f)]
+    public float Distance;
+
+    private float flag;
+    private void Awake()
+    {
+       //PlayerMask =LayerMask.NameToLayer("Player");
+       //Camera.main.cullingMask = (1 << PlayerMask)-1;
+    }
     private void Start()
     {
-        SmoothTime = 0.5f;
 
         Offset = new Vector3(0.0f,5.0f,-8.0f);
 
+        MaxPoint = new Vector3(0.0f,17.5f,-17.5f);
+        MinPoint = new Vector3(0.0f, 1.7f, -1.7f);
+        Distance = 0.5f;
+
         Velocity = Vector3.zero;
 
-        ZoomDistance = 0.0f;
 
         ShakeCamera = false;
 
         Radius = 1.0f;
+
+        flag = 0;
     }
 
     private void Update()
     {
+
+        MouseWheel();
+
+        //Distance 최대값 고정
+        Distance = Mathf.Clamp(Distance,0.0f, 1.0f);
+       
+        float Hor = Input.GetAxis("Horizontal")*-1;
+        
+        //선형 보간
+        if(flag == 0)
+        {
+            transform.position = Vector3.Lerp(
+                Target.transform.position+MaxPoint,
+                Target.transform.position+MinPoint,
+                Distance);
+            flag = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+            flag = 0;
+        transform.RotateAround(Target.transform.position,
+            Vector3.up,
+            Hor * 50.0f * Time.deltaTime);
+
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation,
+            Quaternion.LookRotation((Target.transform.position-transform.position)
+            .normalized),
+            Time.deltaTime);
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
             ShakeCamera = !ShakeCamera;
@@ -48,12 +94,11 @@ public class FollowCamera : MonoBehaviour
                 Random.Range(-0.1f,0.1f) * Radius,
                 0.0f
                 );
-            transform.position = transform.position + Tmp;
+            transform.position += Tmp;
 
         }
 
-        MouseWheel();
-
+        
         if(Input.GetMouseButton(0))
         {
             Vector3 CurrentRotate = transform.rotation.eulerAngles;
@@ -65,42 +110,26 @@ public class FollowCamera : MonoBehaviour
             transform.rotation = Quaternion.Slerp(
                 transform.rotation,
                 CurrentQuaternion,
-                Time.deltaTime * 5.0f*SmoothTime
+                Time.deltaTime * 5.0f
                 );
         }
-        else
-        {
-            transform.rotation = Quaternion.Lerp(
-            transform.rotation,
-            Quaternion.LookRotation(
-                (Target.transform.position - transform.position).normalized),
-            Time.deltaTime * 5.0f * SmoothTime
-            );
-        }
-        Camera.main.fieldOfView = Mathf.Lerp(
-            Camera.main.fieldOfView,
-            ZoomDistance,
-            Time.deltaTime * 5.0f
-            );
-
-        transform.position = Vector3.SmoothDamp(
-            transform.position,
-            Target.transform.position+Offset,
-            ref Velocity,
-            SmoothTime
-            );
-
+        
         void MouseWheel()
         {
-            float Wheel = Input.GetAxis("Mouse ScrollWheel");
+            float Wheel = Input.GetAxis("Mouse ScrollWheel") *-1;
+            if(Wheel!=0)
+            {
+              //Distance = Mathf.Lerp(
+              //    Distance,
+              //    Distance - Wheel*Time.deltaTime,
+              //    100.0f);
+                Distance -= Wheel*Time.deltaTime*10.0f;
 
-            ZoomDistance -= Wheel*10.0f;
-
-            if (ZoomDistance < 20.0f)
-                ZoomDistance = 20.0f;
-
-            if (ZoomDistance > 60.0f)
-                ZoomDistance = 60.0f;
+                transform.position = Vector3.Lerp(
+                Target.transform.position + MaxPoint,
+                Target.transform.position + MinPoint,
+                Distance);
+            }
         }
     }
 }
